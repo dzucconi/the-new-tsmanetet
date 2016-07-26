ccf5236107a6dc3420b85fe562ee70141666c25b
dzucconi <mail@damonzucconi.com>
2016-07-26 17:31:10 +0200

    Accept parsing args

--- parse.rb
+++ parse.rb
@@ -1,3 +1,15 @@
 require './parser'
 
-Parser.parse 'sources/kjvdat.txt', 'output/kjv.txt', 500
+name = ARGV.first
+  .split('/')
+  .last
+  .split('.')
+  .first
+  .downcase
+  .gsub /[^0-9a-z ]/i, '-'
+
+n = ARGV.last.to_i
+n = n.zero? ? nil : n
+
+# eg. Parser.parse 'sources/kjvdat.txt', 'output/kjv.txt', 500
+Parser.parse ARGV.first, "./output/#{name}.txt", n
